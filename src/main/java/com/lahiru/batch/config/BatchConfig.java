fa61af676a11397530e9f3043595977e96fa41b8
LahiruMadushann <lahirumadushandl@gmail.com>
2025-06-17 20:16:46 +0530

    implement student entity

--- src/main/java/com/lahiru/batch/config/BatchConfig.java
+++ src/main/java/com/lahiru/batch/config/BatchConfig.java
@@ -1,10 +1,12 @@
 package com.lahiru.batch.config;
 
+import com.lahiru.batch.student.Student;
 import lombok.RequiredArgsConstructor;
+import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
 public class BatchConfig {
-
+    public FlatFileItemReader<Student> itemReader() {}
 }
