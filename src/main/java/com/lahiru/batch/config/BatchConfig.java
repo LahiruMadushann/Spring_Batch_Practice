8357250913e0f0171dc9abaa9706a84864dba2be
LahiruMadushann <lahirumadushandl@gmail.com>
2025-06-17 21:09:57 +0530

    implement csv reader method

--- src/main/java/com/lahiru/batch/config/BatchConfig.java
+++ src/main/java/com/lahiru/batch/config/BatchConfig.java
@@ -3,10 +3,42 @@ package com.lahiru.batch.config;
 import com.lahiru.batch.student.Student;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
 
 @Configuration
 @RequiredArgsConstructor
 public class BatchConfig {
-    public FlatFileItemReader<Student> itemReader() {}
+
+    @Bean
+    public FlatFileItemReader<Student> itemReader() {
+        FlatFileItemReader<Student> itemReader = new FlatFileItemReader<>();
+        itemReader.setResource(new FileSystemResource("src/main/resources/students.csv"));
+        itemReader.setName("csvReader");
+        itemReader.setLinesToSkip(1);
+        itemReader.setLineMapper(lineMapper());
+        return itemReader;
+    }
+
+    private LineMapper<Student> lineMapper() {
+        DefaultLineMapper<Student> lineMapper = new DefaultLineMapper<>();
+
+        DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
+        lineTokenizer.setDelimiter(",");
+        lineTokenizer.setStrict(false);
+        lineTokenizer.setNames("id", "firstname", "lastname", "age");
+
+        BeanWrapperFieldSetMapper<Student> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
+        fieldSetMapper.setTargetType(Student.class);
+
+        lineMapper.setLineTokenizer(lineTokenizer);
+        lineMapper.setFieldSetMapper(fieldSetMapper);
+
+        return lineMapper;
+    }
 }
