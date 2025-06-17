1217ec8cc7f0124c86cd7c6cc5352827c0edde80
LahiruMadushann <lahirumadushandl@gmail.com>
2025-06-17 20:10:28 +0530

    start implement batch config file

---
+++ src/main/java/com/lahiru/batch/config/BatchConfig.java
@@ -0,0 +1,10 @@
+package com.lahiru.batch.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class BatchConfig {
+
+}
